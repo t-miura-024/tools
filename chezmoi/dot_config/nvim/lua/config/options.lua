d6590ab5e88e2528b19b1a50598af89a071d3062
t-miura-024 <t.miura024@gmail.com>
2026-08-09 19:36:44 +0900

    update: 1 files changed (+14 -0)

--- chezmoi/dot_config/nvim/lua/config/options.lua
+++ chezmoi/dot_config/nvim/lua/config/options.lua
@@ -4,3 +4,17 @@
 
 -- Keep LSP features such as navigation while disabling all diagnostics.
 vim.diagnostic.enable(false)
+
+-- LazyVim enables spell checking for Markdown, but the configured language is English.
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function(args)
+    vim.schedule(function()
+      for _, win in ipairs(vim.api.nvim_list_wins()) do
+        if vim.api.nvim_win_get_buf(win) == args.buf then
+          vim.wo[win].spell = false
+        end
+      end
+    end)
+  end,
+})
