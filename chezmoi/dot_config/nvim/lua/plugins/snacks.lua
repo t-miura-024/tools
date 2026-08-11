c1ead6c0d8654a73d6979eca50cc1bdadb200016
t-miura-024 <t.miura024@gmail.com>
2026-08-11 21:13:27 +0900

    up

---
+++ chezmoi/dot_config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,18 @@
+-- 隠しファイル・gitignore ファイルをエクスプローラーとピッカーで常時表示
+-- （grill rounds 決定: 対象=両方 / 方式=常時表示 / .git 除外 / grep は拡張しない）
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      explorer = {
+        hidden = true,
+        ignored = true,
+        exclude = { ".git/" },
+      },
+    },
+    keys = {
+      { "<leader>ff", LazyVim.pick("files", { hidden = true, ignored = true }), desc = "Find Files (Root Dir)" },
+      { "<leader><space>", LazyVim.pick("files", { hidden = true, ignored = true }), desc = "Find Files (Root Dir)" },
+    },
+  },
+}
